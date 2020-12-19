6b4d5865d85525a8d502384421d32eef3b7c1d64
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-12-19 17:17:42 +0800

    add Eureka service discovery. Error creating bean with name EurekaServerInitializerConfiguration

---
+++ PhotoAppDiscoveryService/src/test/java/com/appsdeveloperblog/photoapp/discovery/photoappdiscoveryservice/PhotoappdiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.discovery.photoappdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappdiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
