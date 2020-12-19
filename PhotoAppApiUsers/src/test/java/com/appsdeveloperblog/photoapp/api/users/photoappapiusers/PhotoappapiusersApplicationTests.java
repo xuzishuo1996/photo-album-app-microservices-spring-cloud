e734f4b9e228f8738382cc25e254a0e7feab6efc
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-12-19 18:27:23 +0800

    add Users microservice project and configure properties

---
+++ PhotoAppApiUsers/src/test/java/com/appsdeveloperblog/photoapp/api/users/photoappapiusers/PhotoappapiusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api.users.photoappapiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapiusersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
