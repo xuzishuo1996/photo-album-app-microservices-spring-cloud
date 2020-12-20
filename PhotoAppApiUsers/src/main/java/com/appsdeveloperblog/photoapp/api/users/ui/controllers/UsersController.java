69486bb4bae1998159464092bceaa735445fc272
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-12-20 16:21:58 +0800

    post return http status code

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -8,6 +8,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -28,7 +30,7 @@ public class UsersController {
     }
 
     @PostMapping
-    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+    public ResponseEntity createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
 
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
@@ -36,6 +38,7 @@ public class UsersController {
         UserDto userDto = modelMapper.map(userDetails, UserDto.class);
         usersService.CreateUser(userDto);
 
-        return "Create user method is called";
+        return new ResponseEntity(HttpStatus.CREATED);
+        // return "Create user method is called";
     }
 }
