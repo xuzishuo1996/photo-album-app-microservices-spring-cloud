652d6de5f83fbbfdbce4062845fc82efbdc5b561
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-12-20 12:15:11 +0800

    use ModelMapper in ServiceImpl to convert from Dto to UserEntity

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
@@ -1,15 +1,36 @@
 package com.appsdeveloperblog.photoapp.api.users.service;
 
+import com.appsdeveloperblog.photoapp.api.users.data.UserEntity;
+import com.appsdeveloperblog.photoapp.api.users.data.UsersRepository;
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class UsersServiceImpl implements UsersService {
+
+    UsersRepository usersRepository;
+
+    @Autowired
+    public UsersServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
     @Override
     public UserDto CreateUser(UserDto userDetails) {
         // Auto-generated method stub
 
         userDetails.setUserId(UUID.randomUUID().toString());
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+
+        usersRepository.save(userEntity);
 
         return null;
     }
