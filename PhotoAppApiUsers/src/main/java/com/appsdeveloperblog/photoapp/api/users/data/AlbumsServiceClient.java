f07e94289efec60f98799d8c29ea07fde256db7e
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-12-27 14:39:42 +0800

    create album feign client in user service

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
@@ -0,0 +1,15 @@
+package com.appsdeveloperblog.photoapp.api.users.data;
+
+import com.appsdeveloperblog.photoapp.api.albums.ui.model.AlbumResponseModel;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name="albums-ws")
+public interface AlbumsServiceClient {
+
+    @GetMapping(path="/users/{id}/albums")
+    List<AlbumResponseModel> getAlbums(@PathVariable("id") String id);
+}
