f8a42b35d5fcd5b0681bbe6ba2cc9e4cecc377ec
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-12-27 16:24:59 +0800

    configure user service to use hystrix circuit breaker and add a fallback

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
@@ -2,14 +2,25 @@ package com.appsdeveloperblog.photoapp.api.users.data;
 
 import com.appsdeveloperblog.photoapp.api.albums.ui.model.AlbumResponseModel;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name="albums-ws")
+@FeignClient(name="albums-ws", fallback=AlbumsFallback.class)
 public interface AlbumsServiceClient {
 
-    @GetMapping(path="/users/{id}/albumss")
+    @GetMapping(path="/users/{id}/albums")
     List<AlbumResponseModel> getAlbums(@PathVariable("id") String id);
+}
+
+@Component
+class AlbumsFallback implements AlbumsServiceClient {
+
+    @Override
+    public List<AlbumResponseModel> getAlbums(String id) {
+        return new ArrayList<>();
+    }
 }
