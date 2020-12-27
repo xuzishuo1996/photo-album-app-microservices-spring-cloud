475981e780d138740fa801bb4fb555cbdf1de3dd
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-12-27 15:16:44 +0800

    enable HTTP request logging in feign client

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.photoapp.api.users;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -27,5 +28,10 @@ public class PhotoAppApiUsersApplication {
     @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLover() {
+        return Logger.Level.FULL;
     }
 }
